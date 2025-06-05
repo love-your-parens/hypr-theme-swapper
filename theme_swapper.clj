5e3fceafbdb9e679a6131dc9303168d2e2df597a
Konrad Wątor <konrad@wator.it>
2025-06-06 01:47:21 +0200

    Add a CLI frontend and a launch-once option

--- theme_swapper.clj
+++ theme_swapper.clj
@@ -1,13 +1,20 @@
 (ns theme-swapper
   (:require [babashka.fs :as fs]
+            [babashka.cli :as cli]
             [clojure.tools.logging :as log]
             [clojure.string :as string]
-            [babashka.process :as process]))
+            [babashka.process :as process]
+            [clojure.edn :as edn]
+            [clojure.java.io :as io]))
 
+(def cli-spec {:spec
+               {:once {:coerce :boolean
+                       :desc "Instructs the program to run once instead of looping."}}})
+(def cli-opts (cli/parse-opts *command-line-args* cli-spec))
 (def sh (comp process/check process/sh))
 (def config-file (fs/file "./config.edn"))
 (def config (or (when (fs/exists? config-file)
-                  (clojure.edn/read (java.io.PushbackReader. (clojure.java.io/reader config-file))))
+                  (edn/read (java.io.PushbackReader. (io/reader config-file))))
                 {}))
 (def wallpaper-directories (get config :wallpaper-directories []))
 (def swap-interval (get config :swap-interval 300))
@@ -22,21 +29,24 @@
 (def wallpapers
   (fs/list-dirs wallpaper-directories picture?))
 
-(defn random-wallpaper
-  []
+(defn random-wallpaper []
   (when (seq wallpapers)
     (rand-nth wallpapers)))
 
 (defn swap-theme []
   (when-let [wallpaper (random-wallpaper)]
-    (try (sh (str "matugen image " wallpaper))
+    (try (sh ["matugen" "image" wallpaper])
          (sh ["systemctl" "--user" "restart" "waybar"])
          (catch Exception e
            (log/warn (.getMessage e))))))
 
 ;;;; Program loop.
-(log/info "Launching the theme swapper. Swap interval:" swap-interval "seconds.")
-(loop []
-  (log/info "Swapping theme...")
+(if (:once cli-opts)
   (swap-theme)
-  (Thread/sleep (* swap-interval 1000)))
+  (do
+    (log/info "Launching the theme swapper. Swap interval:" swap-interval "seconds.")
+    (loop []
+      (log/info "Swapping theme...")
+      (swap-theme)
+      (Thread/sleep (* swap-interval 1000))
+      (recur))))
